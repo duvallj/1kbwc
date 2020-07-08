6039aa4fa9924e214b142627212da6025e0fecf1
Dylan Jones <dylanjones2011@gmail.com>
2020-07-08 00:07:50 -0400

    i m m u t a b i l i z e

---
+++ util.py
@@ -0,0 +1,24 @@
+def immutablize(target):
+    def noset(*args):
+        print("settings")
+        raise RuntimeError()
+    class MetaFactory(type):
+        def __new__(mcls, name, bases, attrs):
+            whitelist = ['__getattribute__', '__setattr__', '__init__']
+            for attr in dir(type(target)):
+                if attr not in whitelist:
+                    attrs[attr] = getattr(type(target), attr)
+            name = type(target).__name__
+            bases += (type(target),)
+            return super(MetaFactory, mcls).__new__(mcls, name, bases, attrs)
+        
+    class Proxy(metaclass=MetaFactory):
+        def __init__(self, obj):
+            object.__setattr__(self, "_backing_obj", obj)
+        def __getattribute__(self, attr):
+            ret = getattr(object.__getattribute__(self, "_backing_obj"), attr)
+            return immutablize(ret)
+        def __setattr__(self, attr, val):
+            raise RuntimeError("urbad")
+    
+    return Proxy(target)
