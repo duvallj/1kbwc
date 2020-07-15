41e9398e3eaad76c8713db01b2cf1f66889b1824
Cheeseitup <cheeseitup24@gmail.com>
2020-07-14 23:44:59 -0400

    Card pack woo!!

---
+++ cards/justified.py
@@ -0,0 +1,10 @@
+from objects import Card, CardFlag
+
+
+class justified(Card):
+    def init(self):
+        self.val = -150
+        self.name = 'J u s t i f i e d'
+        self.image = 'Justified.png'
+        self.flags = set()
+        self.tags = set()
---
+++ cards/urbanization.py
@@ -0,0 +1,13 @@
+from objects import Card, CardFlag
+
+
+class urbanization(Card):
+    def init(self):
+        self.val = 0
+        self.name = 'Urbanization'
+        self.image = 'Urbanization.png'
+        self.flags = {CardFlag.PLAY_ONLY_TO_CENTER}
+        self.tags = set()
+
+    def handle_score_player(self, kernel, player, default_score, gamestate):
+        return -100 * len(player.area.contents)
