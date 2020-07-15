efe08921cf5de78521589413f95553063c4a418b
Alexander Hayden <alexhayden25@gmail.com>
2020-07-15 00:26:15 -0400

    maybe make two cards actually work now?

--- cards/bail_out.py
+++ cards/bail_out.py
@@ -9,13 +9,7 @@ class Bail_Out(Card):
         self.flags = set()
 
     def handle_move(self, kernel, player, card, from_area, to_area, gamestate):
-        print("BO got handle_move!")
-        print(f'{self.owners, player}')
-        print(f'{self.owners[0] == player}')
         if player in self.owners:
-            print(" move is from this card's owner...")
             if from_area == player.hand:
-                print("  and it's from the ower's hand...")
                 if to_area == gamestate.draw:
-                    print("   and it's going to the draw pile.  Allow action!")
                     return True
--- cards/conical_pendulum.py
+++ cards/conical_pendulum.py
@@ -10,11 +10,9 @@ class Conical_Pendulum(Card):
         self.flags = {CardFlag.NO_PLAY_TO_CENTER}
 
     def on_turn_start(self, kernel, player, gamestate):
-        print("CP IS DOING A THING...")
         players = list(gamestate.players.values())
         index = (players.index(self.area.owners[0]) + 1) % len(gamestate.players)
         to_area = players[index].area
-        print(f"{to_area=}")
         self.active = True
         kernel.move_card(self.owners[0], card, self.area, to_area)
         self.active = False
@@ -22,5 +20,4 @@ class Conical_Pendulum(Card):
     def handle_move(self, kernel, player, card, from_area, to_area, gamestate):
         if self.active:
             if card == self:
-                print("CP allowed own move")
                 return True
