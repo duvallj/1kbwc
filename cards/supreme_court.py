f369ec137b7a8190e5db5290847e180e5ecdd918
Dylan Jones <dylanjones2011@gmail.com>
2020-07-15 23:44:42 -0400

    STAPLES. THAT WAS EASY.

---
+++ cards/supreme_court.py
@@ -0,0 +1,18 @@
+from kernel import Kernel
+from objects import Card, Game
+
+
+class SupremeCourt(object): # not a card
+    def init(self):
+        self.val = 300
+        self.name = 'Supreme Court'
+        self.image = "Supreme_Court.png"
+        self.flags = set()
+
+    def on_end_game(self, kernel: Kernel, gamestate: Game):
+        raise NotImplementedError("URBAD")
+        # URBAD
+        scores = {name: kernel.score_player(p) for name, p in gamestate.players.values()}
+        for p, score in scores.values():
+            #nearby = [p for p in]
+            pass
