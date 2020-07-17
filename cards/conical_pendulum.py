17f891d223eb8b581fd26928c7b0409b302c9615
Alexander Hayden <alexhayden25@gmail.com>
2020-07-16 21:44:49 -0400

    remove print from conical pendulum

--- cards/conical_pendulum.py
+++ cards/conical_pendulum.py
@@ -11,10 +11,7 @@ class Conical_Pendulum(Card):
 
     def on_turn_start(self, kernel, player, gamestate):
         self.turn_starts += 1
-        print(f"{gamestate.turn_num} {self.turn_starts}")
-        print("pendulum doin what it do")
         players = list(gamestate.players.values())
         index = (players.index(self.area.owners[0]) + 1) % len(gamestate.players)
         to_area = players[index].area
-        print(kernel.move_card(self, self, self.area, to_area))
 
